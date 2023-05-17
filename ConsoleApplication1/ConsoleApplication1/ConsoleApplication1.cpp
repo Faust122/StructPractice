ef8075b5cec0febf85fff17dbe620cdb522647c0
Faust122 <119011975+Faust122@users.noreply.github.com>
2023-05-17 13:24:40 +0300

    create  function Sort by rating in genre

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -45,9 +45,28 @@ int FindByGenre(Films& obj, string value, int size)
     }
     return -1;
 }
-
+int FindByRatingInGenre(Films&* arr,Films& obj,string value, int size, int rating)
+{
+    int tmp;
+    tmp = arr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        if (obj.genre == value )
+        {
+            for (size_t j = 0; j < size; i++)
+            {
+                if (tmp<arr[j])
+                {
+                    tmp = arr[j];
+                }
+            }
+        }
+    }
+         return tmp;
+}
 int main()
 {
-        
+    int size = 20;
+    Films* library = new Films[size];
 }
 
