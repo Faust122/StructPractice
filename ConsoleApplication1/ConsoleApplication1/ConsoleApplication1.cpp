d942ff3fc30e102361ad290b646d8a649beb1384
Faust122 <119011975+Faust122@users.noreply.github.com>
2023-05-17 13:01:56 +0300

    crate  search by name

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,9 +10,19 @@ struct Films
     float rating;
     float cost;
 };
-
+int FindByName(Films& obj,string value,int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if ( obj.NameFilms == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 int main()
 {
-    
+        
 }
 
