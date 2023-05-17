053e8f235c6c93b83e3b3861e1eb18194a6708d8
Faust122 <semeniuk_ak22@nuwm.edu.ua>
2023-05-17 12:45:52 +0300

    create struct

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+struct Films
+{
+    string NameFilms;
+    string director;
+    string genre;
+    float rating;
+    float cost;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
